909897ee4915a0adc704b2defe2829cf01f1e27a
akimoovv <63498188+akimoovv@users.noreply.github.com>
2020-04-11 20:41:51 +0500

    Update Устойчивый автоконтраст черно-белого изображения.py

--- Brightness and color correction(week 3)/Устойчивый автоконтраст черно-белого изображения.py
+++ Brightness and color correction(week 3)/Устойчивый автоконтраст черно-белого изображения.py
@@ -1,19 +1,19 @@
 from skimage.io import imread, imsave, imshow
 from numpy import histogram, clip
 
-#img = imread('https://stepik.org/media/attachments/lesson/58402/tiger-low-contrast.png') 
+
 img = imread('img.png') 
 h, bin_edges = histogram(img, bins=range(257)) 
-k = round(img.size * 0.05) # Отбрасываем 5% пикселей
+k = round(img.size * 0.05)
 count = 0
 
-for i in range(256): # Бежим по гистограмме и ищем минимум гистограммы
+for i in range(256): 
     count += h[i]
     if count > k:
         x_min = i
         count = 0
         break
-for i in range(255,0,-1): # Бежим по гистограмме и ищем максимум гистограммы
+for i in range(255,0,-1): 
     count += h[i]
     if count > k:
         x_max = i
