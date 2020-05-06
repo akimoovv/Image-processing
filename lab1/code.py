ac7d5a5a6de0ead8f3b55974411bca6752788db8
Иван Акимов <63498188+akimoovv@users.noreply.github.com>
2020-05-06 23:52:35 +0500

    Update code.py

--- lab1/code.py
+++ lab1/code.py
@@ -1,20 +1,25 @@
-def code(img, n):
-    img_yuv = rgb_to_yuv(img) # преобразовываем изображение в yuv
+def code(img, n=2): # Работает правильно
+    img_f = img_as_float(img)
     
-    res = img_yuv.copy()
+    r = img_f[:,:,0]
+    g = img_f[:,:,1]
+    b = img_f[:,:,2]
+    
+    y = 0.299 * r  + 0.587 * g  + 0.114 * b
+    u = u2 = 0 - 0.1687 * r - 0.3313 * g + 0.5 * b + 0.5
+    v = v2 = 0.5 * r - 0.4187 * g - 0.0813 * b + 0.5
     #Итерируемся по изображению и каждый пиксель "квадрата" размера n^2 делаем равным левому верхнему элементу квадрата
     #То есть делаем децимацию
-    for i in range(0,res.shape[0],n):
-        for j in range(0,res.shape[1],n):
+    for i in range(0,u.shape[0],n):
+        for j in range(0,u.shape[1],n):
             for k1 in range(n):
                 for k2 in range(n): 
-                    try: res[i + k1,j + k2, 1] = img_yuv[i,j,1]; res[i + k1,j + k2, 2] = img_yuv[i,j,2]; 
+                    try: u[i + k1,j + k2] = u2[i,j]; v[i + k1,j + k2] = v2[i,j]; 
                     except: pass 
                     
                     
-    y = res[:,:,0]
-    cb = res[:,:,1]
-    cr = res[:,:,2]    
+    y = y
+    Cb = u
+    Cr = v  
     
-    
-    return y,cb,cr 
+    return y,Cb,Cr
